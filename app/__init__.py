c3467744862502c4211f8a0a730d02d21c4fc69b
Devon-Wilkening <devon.wilkening1@gmail.com>
2025-03-11 20:39:29 -0500

    Set up Flask-Migrate for easy migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,32 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from flask_migrate import Migrate
 
+# Initialize db and migrate instances
 db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    app.config.from_object(Config)
+    
+    # Initialize db and migrate with the app
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        # Import models to register them with the database
+        from . import models  # Ensure models are imported so tables are created
+        # No need to call db.create_all() here if using migrations
+
+        # Import and register routes
+        from .routes import init_routes
+        init_routes(app)
+
+    return app
+
+'''db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
@@ -17,6 +41,6 @@ def create_app():
         from .routes import init_routes
         init_routes(app)
 
-    return app
+    return app'''
 
 
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Devon\Desktop\CS496\CS-496-Project\venv
+set VIRTUAL_ENV=C:\Users\devon\OneDrive\Desktop\CS496\CS-496-Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
